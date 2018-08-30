46ffe4777cfdb8f040e3d3c7d22d4c23ff95d266
Jezrien Hsieh <jtr860830@yahoo.com.tw>
2018-08-30 22:25:34 +0800

    Last commit forget to add

---
+++ app/src/main/java/com/example/user/navigation_calendar/EventItemTouchHelper.java
@@ -0,0 +1,64 @@
+package com.example.user.navigation_calendar;
+
+import android.graphics.Canvas;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+
+public class EventItemTouchHelper extends ItemTouchHelper.SimpleCallback {
+
+    private EventItemTouchHelperListener listener;
+
+    public EventItemTouchHelper(int dragDirs, int swipeDirs, EventItemTouchHelperListener listener) {
+        super(dragDirs, swipeDirs);
+        this.listener = listener;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder viewHolder1) {
+        return true;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        if (listener != null)
+            listener.onSwiped(viewHolder, direction, viewHolder.getAdapterPosition());
+    }
+
+    @Override
+    public void clearView(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        View foregroundView = ((eventAdapter.ViewHolder)viewHolder).viewForeground;
+        getDefaultUIUtil().clearView(foregroundView);
+    }
+
+    @Override
+    public int convertToAbsoluteDirection(int flags, int layoutDirection) {
+        return super.convertToAbsoluteDirection(flags, layoutDirection);
+    }
+
+    @Override
+    public void onSelectedChanged(@Nullable RecyclerView.ViewHolder viewHolder, int actionState) {
+        if (viewHolder != null) {
+            View foregroundView = ((eventAdapter.ViewHolder)viewHolder).viewForeground;
+            getDefaultUIUtil().onSelected(foregroundView);
+        }
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        View foregroundView = ((eventAdapter.ViewHolder)viewHolder).viewForeground;
+        getDefaultUIUtil().onDraw(c, recyclerView, foregroundView, dX, dY, actionState, isCurrentlyActive);
+    }
+
+    @Override
+    public void onChildDrawOver(@NonNull Canvas c, @NonNull RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        View foregroundView = ((eventAdapter.ViewHolder)viewHolder).viewForeground;
+        getDefaultUIUtil().onDrawOver(c, recyclerView, foregroundView, dX, dY, actionState, isCurrentlyActive);
+    }
+}
+
+interface EventItemTouchHelperListener {
+    void onSwiped(RecyclerView.ViewHolder viewHolder, int direction, int position);
+}
