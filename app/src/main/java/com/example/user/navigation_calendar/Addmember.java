66147d0a844b1222cc747bbcd23eaf50191445ee
Jezrien Hsieh <jtr860830@yahoo.com.tw>
2018-09-02 15:23:26 +0800

    Fix dialog issue

--- app/src/main/java/com/example/user/navigation_calendar/Addmember.java
+++ app/src/main/java/com/example/user/navigation_calendar/Addmember.java
@@ -1,6 +1,6 @@
 package com.example.user.navigation_calendar;
 
-import android.app.DialogFragment;
+import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
