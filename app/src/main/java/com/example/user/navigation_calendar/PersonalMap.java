bc6bdc126c429743b5fa7d45cb6fd71e027cff35
Isabel <isabel1997517@gmail.com>
2018-09-10 23:29:51 +0800

    personal map spinner

--- app/src/main/java/com/example/user/navigation_calendar/PersonalMap.java
+++ app/src/main/java/com/example/user/navigation_calendar/PersonalMap.java
@@ -13,9 +13,24 @@ public class PersonalMap extends AppCompatActivity implements View.OnClickListen
 
     ImageButton back;
     //下拉式選單
-    private String[] category_list = {"Business","Gathering","Dinner","Travel","Others"}; //宣告字串陣列
-    private ArrayAdapter<String> category_listAdapter; //喧告listAdapter物件
-    Spinner category;
+    private String[] Pcategory_list = {"Business","Gathering","Dinner","Travel","Others"}; //宣告字串陣列
+    private ArrayAdapter<String> Pcategory_listAdapter; //喧告listAdapter物件
+    Spinner Pcategory;
+
+    private String[] Pweek_list = {"1 week","2 week","3 week"}; //宣告字串陣列
+    private ArrayAdapter<String> Pweek_listAdapter; //喧告listAdapter物件
+    Spinner Pweek;
+
+    //宣告字串陣列
+    private String[] Pmonth_list = {"1 month","2 month","3 month","4 month","5 month","6 month",
+            "7 month","8 month","9 month","10 month","11 month","12 month"};
+    private ArrayAdapter<String> Pmonth_listAdapter; //喧告listAdapter物件
+    Spinner Pmonth;
+
+    //宣告字串陣列
+    private String[] Pyear_list = {"1 year","2 year","3 year","4 year","5 year"};
+    private ArrayAdapter<String> Pyear_listAdapter; //喧告listAdapter物件
+    Spinner Pyear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,39 +40,94 @@ public class PersonalMap extends AppCompatActivity implements View.OnClickListen
         back=findViewById(R.id.pm_back);
         back.setOnClickListener(this);
         category_menu();
-
+        getPMSpinnerItem();
 
     }
 
     public void category_menu(){
         //選擇"週"的下拉式選單
-        category=findViewById(R.id.pm_spinner);
+        Pcategory=findViewById(R.id.pm_spinner);
         //建立一個ArrayAdapter物件，並放置下拉選單的內容
-        category_listAdapter = new ArrayAdapter<String>(this, R.layout.myspinner,category_list);
+        Pcategory_listAdapter = new ArrayAdapter<String>(this, R.layout.myspinner,Pcategory_list);
         //設定下拉選單的樣式
-        category_listAdapter.setDropDownViewResource(R.layout.myspinner_list);//android.R.layout.simple_spinner_dropdown_item
-        category.setAdapter(category_listAdapter);
+        Pcategory_listAdapter.setDropDownViewResource(R.layout.myspinner_list);//android.R.layout.simple_spinner_dropdown_item
+        Pcategory.setAdapter(Pcategory_listAdapter);
         //設定項目被選取之後的動作
-        category.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+        Pcategory.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
             public void onItemSelected(AdapterView adapterView, View view, int position, long id){
                 Toast.makeText(PersonalMap.this, "您選擇"+adapterView.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
                 if (adapterView.getSelectedItem().toString() == "Business"){
-                    category.setBackgroundColor(getResources().getColor(R.color.block1));
+                    Pcategory.setBackgroundColor(getResources().getColor(R.color.block1));
                 }else if (adapterView.getSelectedItem().toString() == "Gathering"){
-                    category.setBackgroundColor(getResources().getColor(R.color.block2));
+                    Pcategory.setBackgroundColor(getResources().getColor(R.color.block2));
                 }else if (adapterView.getSelectedItem().toString() == "Dinner"){
-                    category.setBackgroundColor(getResources().getColor(R.color.block3));
+                    Pcategory.setBackgroundColor(getResources().getColor(R.color.block3));
                 }else if (adapterView.getSelectedItem().toString() == "Travel"){
-                    category.setBackgroundColor(getResources().getColor(R.color.block4));
+                    Pcategory.setBackgroundColor(getResources().getColor(R.color.block4));
                 }else if (adapterView.getSelectedItem().toString() == "Others"){
-                    category.setBackgroundColor(getResources().getColor(R.color.block5));
+                    Pcategory.setBackgroundColor(getResources().getColor(R.color.block5));
                 }
 
             }
             public void onNothingSelected(AdapterView arg0) {
-                Toast.makeText(PersonalMap.this, "您沒有選擇任何項目", Toast.LENGTH_LONG).show();
+                Toast.makeText(PersonalMap.this, "您沒有選擇任何項目", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public void getPMSpinnerItem(){
+
+        //選擇"週"的下拉式選單
+        Pweek=findViewById(R.id.pm_spinnerweek);
+        //建立一個ArrayAdapter物件，並放置下拉選單的內容
+        Pweek_listAdapter = new ArrayAdapter<String>(this, R.layout.myspinner, Pweek_list);
+        //設定下拉選單的樣式
+        Pweek_listAdapter.setDropDownViewResource(R.layout.myspinner_list);//android.R.layout.simple_spinner_dropdown_item
+        Pweek.setAdapter(Pweek_listAdapter);
+        //設定項目被選取之後的動作
+        Pweek.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+            public void onItemSelected(AdapterView adapterView, View view, int position, long id){
+                Toast.makeText(PersonalMap.this, "您選擇"+adapterView.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+            }
+            public void onNothingSelected(AdapterView arg0) {
+                Toast.makeText(PersonalMap.this, "您沒有選擇任何項目", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //選擇"月"的下拉式選單
+        Pmonth=findViewById(R.id.pm_spinnermonth);
+        //建立一個ArrayAdapter物件，並放置下拉選單的內容
+        Pmonth_listAdapter = new ArrayAdapter<String>(this, R.layout.myspinner, Pmonth_list);
+        //設定下拉選單的樣式
+        Pmonth_listAdapter.setDropDownViewResource(R.layout.myspinner_list);//android.R.layout.simple_spinner_dropdown_item
+        Pmonth.setAdapter(Pmonth_listAdapter);
+        //設定項目被選取之後的動作
+        Pmonth.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+            public void onItemSelected(AdapterView adapterView, View view, int position, long id){
+                Toast.makeText(PersonalMap.this, "您選擇"+adapterView.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+            }
+            public void onNothingSelected(AdapterView arg0) {
+                Toast.makeText(PersonalMap.this, "您沒有選擇任何項目", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //選擇"年"的下拉式選單
+        Pyear=findViewById(R.id.pm_spinneryear);
+        //建立一個ArrayAdapter物件，並放置下拉選單的內容
+        Pyear_listAdapter = new ArrayAdapter<String>(this, R.layout.myspinner, Pyear_list);
+        //設定下拉選單的樣式
+        Pyear_listAdapter.setDropDownViewResource(R.layout.myspinner_list);//android.R.layout.simple_spinner_dropdown_item
+        Pyear.setAdapter(Pyear_listAdapter);
+        //設定項目被選取之後的動作
+        Pyear.setOnItemSelectedListener(new Spinner.OnItemSelectedListener(){
+            public void onItemSelected(AdapterView adapterView, View view, int position, long id){
+                Toast.makeText(PersonalMap.this, "您選擇"+adapterView.getSelectedItem().toString(), Toast.LENGTH_LONG).show();
+            }
+            public void onNothingSelected(AdapterView arg0) {
+                Toast.makeText(PersonalMap.this, "您沒有選擇任何項目", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 
     @Override
