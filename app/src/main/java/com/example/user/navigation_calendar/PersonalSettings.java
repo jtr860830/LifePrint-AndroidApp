7450f0343d7d864feb0667c14f6f2899be29cadf
Isabel <isabel1997517@gmail.com>
2018-09-12 18:19:57 +0800

    personal setting photo

--- app/src/main/java/com/example/user/navigation_calendar/PersonalSettings.java
+++ app/src/main/java/com/example/user/navigation_calendar/PersonalSettings.java
@@ -1,12 +1,17 @@
 package com.example.user.navigation_calendar;
 
 import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -28,10 +33,11 @@ import android.widget.ToggleButton;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PersonalSettings extends AppCompatActivity implements View.OnClickListener {
+public class PersonalSettings extends AppCompatActivity implements View.OnClickListener{
 
     private RecyclerView recyclerView;
     private PersonalSettingsAdapter adapter;
@@ -39,6 +45,8 @@ public class PersonalSettings extends AppCompatActivity implements View.OnClickL
     ImageButton ps_save;
     ImageButton ps_clean1;
     ImageButton ps_clean2;
+    ImageButton change_pic;
+    ImageView personal_pic;
 
     EditText email;
     EditText birthday;
@@ -111,6 +119,12 @@ public class PersonalSettings extends AppCompatActivity implements View.OnClickL
         ps_clean2=findViewById(R.id.psbtn_clean2);
         ps_clean2.setOnClickListener(this);
 
+        //找Change Picture Button按鈕
+        change_pic=findViewById(R.id.btn_changePP1);
+        personal_pic=findViewById(R.id.per_pic);
+        //設定按鈕監聽
+        change_pic.setOnClickListener(this);
+
         //set token
         NsharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
         token = NsharedPreferences.getString("TOKEN", "");
@@ -170,8 +184,65 @@ public class PersonalSettings extends AppCompatActivity implements View.OnClickL
                 email=findViewById(R.id.ps_email);
                 email.setText("");
                 break;
+
+            case R.id.btn_changePP1:
+                Intent intent = new Intent();
+                //開啟Pictures畫面Type設定為image
+                intent.setType("image/*");
+                //使用Intent.ACTION_GET_CONTENT這個Action                                            //會開啟選取圖檔視窗讓您選取手機內圖檔
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                //取得相片後返回本畫面
+                startActivityForResult(intent, 2);
+                break;
         }
     }
+
+    //取得相片後返回的監聽式
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //當使用者按下確定後
+        if (resultCode == RESULT_OK){
+            //取得圖檔的路徑位置
+            Uri uri = data.getData();
+            //寫log
+            Log.e("uri", uri.toString());
+            //抽象資料的接口
+            ContentResolver cr = this.getContentResolver();
+            try {
+                //由抽象資料接口轉換圖檔路徑為Bitmap
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(uri));
+                //取得圖片控制項ImageView
+                ImageView imageView = (ImageView) findViewById(R.id.per_pic);
+                // 將Bitmap設定到ImageView
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                Log.e("Exception", e.getMessage(),e);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    protected void doCropPhoto(Uri uri){
+        //進行照片裁剪
+        Intent intent = getCropImageIntent(uri);
+        startActivityForResult(intent, 2);
+    }
+    //裁剪圖片的Intent設定
+    public static Intent getCropImageIntent(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri,"image/*");
+        intent.putExtra("crop", "true");// crop=true 有這句才能叫出裁剪頁面.
+        intent.putExtra("scale", true); //讓裁剪框支援縮放
+        intent.putExtra("aspectX", 1);// 这兩項為裁剪框的比例.
+        intent.putExtra("aspectY", 1);// x:y=1:1
+        intent.putExtra("outputX", 500);//回傳照片比例X
+        intent.putExtra("outputY", 500);//回傳照片比例Y
+        intent.putExtra("return-data", true);
+        return intent;
+    }
+
+
 }
 //JSON-->data
 class PerSetCard {
